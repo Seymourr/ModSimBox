1e523244002a9b3ff113afb2c99471e66bb01a7e
Seymourr <jack_shabo6@hotmail.com>
2015-02-24 14:23:41 +0100

    hi

---
+++ Assets/Fall.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Fall : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Translate(Vector3.down * 3.0f * Time.deltaTime, Space.World);
+
+	}
+}
